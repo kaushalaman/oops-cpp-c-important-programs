a55d74513bbc07362358c3ccf349934c34b27b17
Aman Kaushal <kaushalaman02@gmail.com>
2015-07-16 01:39:40 +0530

    Kadane’s Algorithm added largest sum contiguos subarray

--- array/max_contiguos_sum_array.cpp
+++ array/max_contiguos_sum_array.cpp
@@ -12,10 +12,26 @@ int max_sum_subarray(int A[], int arr_size){
 	return max_so_far;
 }
 
+int max_sum_subarray_2(int A[],int arr_size){
+	int max_so_far=0;
+	int max_ending=0;
+	for(int i=0;i<=arr_size-1;i++){
+		max_ending=max_ending+A[i];
+		if(max_ending<0){
+			max_ending=0;
+		}
+		else if(max_ending>max_so_far){
+			max_so_far=max_ending;
+		}
+	}
+	return max_so_far;
+}
+
 int main(int argc, char const *argv[])
 {
 	/* code */
 	int a[]={-2, -3, 4, -1, -2, 1, 5, -3};
-	cout<<max_sum_subarray(a,8);
+	cout<<max_sum_subarray(a,8)<<endl;
+	cout<<max_sum_subarray_2(a,8)<<endl;
 	return 0;
 }
