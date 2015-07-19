566fb4bab2b97fea7477d33d22240f7e6c8d5590
Aman Kaushal <kaushalaman02@gmail.com>
2015-07-19 17:04:21 +0530

    constructors and initilization list for constuctor in derived class

---
+++ oops/inheritance/constructors_derived.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+class alpha
+{
+	int x;
+public:
+	alpha(int i)
+	{
+		x=i;
+		cout<<"alpha initialized"<<endl;
+	}
+
+	void show_x(void)
+	{
+		cout<<"x= "<<x<<endl;
+	}
+};
+
+class beta
+{
+	int y;
+public:
+	beta(int i)
+	{
+		y=i;
+		cout<<"beta initialized"<<endl;
+	}
+
+	void show_y(void)
+	{
+		cout<<"y= "<<y<<endl;
+	}
+};
+
+class gamma:public beta,public alpha
+{
+	int m,n;
+public:
+	gamma(int a,int b,int i, int j):alpha(a),beta(b)
+	{
+		m=i;
+		n=j;
+		cout<<"gamma initialized "<<endl;
+	}
+
+	void show_mn(void)
+	{
+		cout<<"m= "<<m<<endl;
+		cout<<"n= "<<n<<endl;
+	}
+};
+
+int main()
+{
+	gamma g(5,10,20,30);
+	g.show_x();
+	g.show_y();
+	g.show_mn();
+	return 0;
+}
