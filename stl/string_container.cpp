ddd63667bdc623171624f01f403e602c6246da81
Aman Kaushal <kaushalaman02@gmail.com>
2015-07-23 10:11:24 +0530

    string container added

---
+++ stl/string_container.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <cstdlib>
+#include <cstdio>
+using namespace std;
+int main()
+{
+	string s,str="This is a text example";
+	int found;
+	cout<<str.size()<<endl;
+	cout<<str.length()<<endl;
+	str.insert(3,"am");
+	cout<<str<<endl;
+	str.erase(3,2);
+	cout<<str<<endl;
+	str.append("dhud");
+	cout<<str<<endl;
+	cout<<"enter substring to be found"<<endl;
+	cin>>s;
+	found=str.find(s);
+	cout<<"found"<<found<<endl;
+	for(string::iterator it=str.begin();it!=str.end();it++)
+	{
+		cout<<*it<<" ";
+	}
+	cout<<endl;
+	str[1]=toupper(str[1]);
+	cout<<str<<endl;
+	if(str.find("ami")!=string::npos)
+	{
+		cout<<"found"<<endl;
+	}
+	else{
+		cout<<"not found"<<endl;
+	}
+	return 0;
+
+}
