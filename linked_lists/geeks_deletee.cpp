b4ad41a9ac2071ddd2abff699e8492d52b058485
Aman Kaushal <kaushalaman02@gmail.com>
2015-07-29 19:26:46 +0530

    detect_loop, middle,reverse added

--- linked_lists/geeks_deletee.cpp
+++ linked_lists/geeks_deletee.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
+#include <vector>
 using namespace std;
 
 struct node{
 	int data;
 	struct node *next;
+	bool visited=false;
 };
 
 typedef struct node Node;
@@ -27,6 +29,64 @@ void front_push(Node **head_ref,int data)
 	Node *temp=New(data);
 	temp->next=*head_ref;
 	*head_ref=temp;
+}
+int detect_loop(Node **head_ref)
+{
+	if(*head_ref==NULL)
+	{
+		cout<<"no loop"<<endl;
+		return 0;
+	}
+	Node *ptr=*head_ref;
+
+	while(1)
+	{
+		ptr->visited=true;
+		ptr=ptr->next;
+		if(ptr->visited)
+		{
+			cout<<"loop is detected at "<<ptr->data<<endl;
+			return 0;
+		}
+	}
+
+	
+}
+void middle_element(Node **head_ref)
+{
+	if(*head_ref==NULL)
+	{
+		cout<<"not found middle element"<<endl;
+		return;
+	}	
+	Node *ptr1=*head_ref;
+	if(!ptr1){
+		return;
+	}
+	if(ptr1->next==NULL)
+	{
+		return;
+	}
+	Node *ptr2=*head_ref;
+	if(!ptr2){
+		return;
+	}
+	if(ptr1==*head_ref && ptr2->next->next==NULL)
+	{
+		cout<<"middle node value is "<<ptr1->data<<endl;
+		return;
+	}
+	while(ptr2->next != NULL)
+	{
+		ptr2=ptr2->next->next;
+		ptr1=ptr1->next;
+		if(ptr2==NULL){
+			break;
+		}
+	}
+	cout<<"middle node value is "<<ptr1->data<<endl;
+	return;
+
 }
 void back_push(Node **head_ref,int data){
 	
@@ -135,7 +195,92 @@ void node_delete(Node **head_ref,int val)
 	}
 	return;
 }
+void nth_node_back(Node **head_ref,int n)
+{
+	Node *current=*head_ref;
+	int count=0;
+	while(current!=NULL){
+		count++;
+		current=current->next;
+	}
+	if(n>count)
+	{
+		cout<<"Not so list items"<<endl;
+		return;
+	}
+	int m=(count-n)+1;
+	current=*head_ref;
+	while(m!=1)
+	{
+		current=current->next;
+		m--;
+	}
+	cout<<current->data<<endl;
+	return;
+}
 
+void reverse(Node **head_ref){
+	Node *prev,*link;
+	Node *current=*head_ref;
+	prev=current;
+	link=current->next;
+	current=link;
+	prev->next=NULL;
+	while(current!=NULL)
+	{
+		link=current->next;
+		current->next=prev;
+		prev=current;
+		current=link;
+	}
+	*head_ref=prev;
+}
+
+void delete_list(Node **head_ref)
+{
+	Node *current=*head_ref;
+	Node *temp=NULL;
+	while(current!=NULL)
+	{
+		temp=current;
+		current=current->next;
+		delete temp;
+	}
+	*head_ref=NULL;
+	cout<<"List is deleted"<<endl;
+	return;
+}
+
+void delete_list_single(Node **head_ref)
+{
+	Node *current=*head_ref;
+	*head_ref=NULL;
+	Node *temp=NULL;
+	while(current!=NULL)
+	{
+		temp=current;
+		current=current->next;
+		delete temp;
+	}
+	cout<<"List is deleted"<<endl;
+	return;
+}
+
+void count_int(Node **head_ref,int val)
+{
+	Node *current=*head_ref;
+	int count=0;
+	while(current!=NULL)
+	{
+		if(current->data==val){
+			count++;
+		}
+		current=current->next;
+
+	}
+	cout<<count<<endl;
+	return;
+}
 void delete_pointer_node(Node *ptr)
 {
 	ptr->data=ptr->next->data;
@@ -171,6 +316,11 @@ int main()
 	back_push(&head,3);
 	back_push(&head,4);
 	front_push(&head,0);
+	/*Node *ptr=head;
+	Node *ptr1;
+	ptr1=ptr->next->next->next;
+	ptr1->next->next=ptr1;
+	detect_loop(&head);*/
 	insert_after(head->next,1);
 	print(head);
 	node_delete(&head,0);
@@ -179,9 +329,17 @@ int main()
 	cout<<search(head,2)<<endl;
 	cout<<search(head,3)<<endl;
 	getnth(head,0);
-	Node *ptr=head; 
-	cout<<ptr->next->next->next->data<<endl;
-	delete_pointer_node(ptr->next->next->next);
+	Node *ptr2=head; 
+	cout<<ptr2->next->next->next->data<<endl;
+	delete_pointer_node(ptr2->next->next->next);
+	print(head);
+	middle_element(&head);
+	nth_node_back(&head,5);
+	count_int(&head,1);
+	count_int(&head,2);
+	reverse(&head);
+	print(head);
+	delete_list_single(&head);
 	print(head);
 	return 0;
 }
