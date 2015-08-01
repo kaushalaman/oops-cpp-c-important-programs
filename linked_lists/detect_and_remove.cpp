dd5b9f46435e078bf68f5bce65daa88ed50db07a
Aman Kaushal <kaushalaman02@gmail.com>
2015-08-01 21:01:29 +0530

    detect and remove loop in linkedlist

--- linked_lists/detect_and_remove.cpp
+++ linked_lists/detect_and_remove.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-#include <cstdlib>
 #include <cstdio>
+#include <cstdlib>
 using namespace std;
 
 struct node{
 	int data;
 	struct node *next;
+	bool visited;
 };
 
 typedef struct node Node;
@@ -13,16 +14,30 @@ typedef struct node Node;
 Node *New(int val)
 {
 	Node *temp=new node;
-	if(!temp)
-	{
-		cout<<"not done"<<endl;
-		return NULL;
-	}
 	temp->data=val;
 	temp->next=NULL;
 	return temp;
-}
-void front_push(Node **head_ref,int data)
+} 
+
+void detect_remove(Node **head_ref)
+{
+	if(*head_ref == NULL)
+	{
+		cout<<"not a loop found"<<endl;
+		return;
+	}
+	Node *ptr1=*head_ref;
+	while(ptr1->next!=NULL)
+	{
+		ptr1->visited=true;
+		if(ptr1->next->visited!=true)
+			ptr1=ptr1->next;
+		else{
+			ptr1->next=NULL;
+			return;
+		}
+	}
+}void front_push(Node **head_ref,int data)
 {
 	Node *temp=New(data);
 	temp->next=*head_ref;
@@ -37,7 +52,8 @@ void back_push(Node **head_ref,int data){
 		return;
 	}
 	Node *start=*head_ref;
-hhhh		Node *temp=New(data);
+	if(start->next==NULL){
+		Node *temp=New(data);
 		start->next=temp;
 		return;
 	}
@@ -49,17 +65,6 @@ hhhh		Node *temp=New(data);
 		start->next=temp;
 	}
 	return;
-}
-
-void insert_after(Node *prev_node,int data)
-{
-	if(prev_node->next==NULL)
-	{
-		return;
-	}
-	Node *temp=New(data);
-	temp->next=prev_node->next;
-	prev_node->next=temp;
 }
 
 void print(Node *head_ref)
@@ -83,10 +88,35 @@ void print(Node *head_ref)
 int main()
 {
 	Node *head=NULL;
+	Node *prev;
 	back_push(&head,1);
 	back_push(&head,2);
-	front_push(&head,0);
-	insert_after(head->next,1);
+	back_push(&head,3);
+	back_push(&head,4);
+	back_push(&head,5);
+	back_push(&head,6);
+	back_push(&head,7);
+	Node *start=head;
+	while(start->data!=4)
+	{
+		start=start->next;
+	}
+	Node *start1=head;
+	while(start1->data!=7)
+	{
+		start1=start1->next;
+	}
+	start1->next=start;
+	//print(head);
+	/*Node *ptr=head;
+	Node *ptr1;
+	ptr1=ptr->next->next->next;
+	ptr1->next->next=ptr1;
+	detect_loop(&head);*/
+	detect_remove(&head);
 	print(head);
 	return 0;
 }
+
+
+	
